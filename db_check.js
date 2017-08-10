04caa8b6ad5ecb0151a51e09e4c2a7465cca9c2e
Mecca Parker <Mecca@mjp.local>
2017-08-10 14:38:32 -0400

    daemon bug fixes

--- db_check.js
+++ db_check.js
@@ -5,6 +5,7 @@ var DBDaemon = function() {
 	var id = parseInt(process.argv[2])
 	var power = parseInt(process.argv[3])
 	var source = parseInt(process.argv[4])
+	var temperature = parseInt(process.argv[5])
 
 	// Initialize
 	self.init = function() {
@@ -13,19 +14,25 @@ var DBDaemon = function() {
 			if (err) {
 				console.log("Could not update database.")
 				console.log(err)
+				process.exit(1)
 			}
 			else {
 				console.log("Database successfully updated.")
+				process.exit(1)
 			}
-			process.exit(1)
 		})
 	}
 	
 	// Update database with new values
 	self.updateDatabase = function(completion) {
-		var query = "UPDATE hub SET power = $2, source = $3 WHERE id = $1"
+
+		var query = "UPDATE hub SET power = $2, source = $3 WHERE id = $1;" + 
+
+					"UPDATE pi SET temperature = $4" +
+					"WHERE id = $1"
+
 		console.log('Attempting to update database...')
-		db.none(query, [id, power, source])
+		db.none(query, [id, power, source, temperature])
 		.then(function() {
 			completion(null)
 			})
--- sp_daemon.py
+++ sp_daemon.py
@@ -4,88 +4,131 @@ import sys
 import os
 import datetime
 import time
+import json
 from config import config_daemon as config
 import serial_handler
+import pi_daemon
+import subprocess
 
 # Main function responsible for gathering info and updating database
-def runUpdate():
-	startTime = time.time()
-	ID, serial_port, baud_rate, timeout = config.configure()
-	print("#############################################")
-	print(datetime.datetime.now())
-	print("ID: %d, SERIAL_PORT: %s, BAUDRATE: %d, TIMEOUT: %0.2f" % (ID, serial_port, baud_rate, timeout))
-	print("Database update for Hub with ID %d initiated." % ID)
-	getHubData(ID, serial_port, baud_rate, timeout)
-	endTime = time.time()
-	totalTime = endTime - startTime
-	print("[completed in %.1fs]" % totalTime)
-	print("#############################################")
 
-# Open serial port and query information
-def getHubData(ID, serial_port, baud_rate, timeout):
-	print("Attempting to retrieve Hub data...")
-	try: 
-		def getData():
-			power, source = "", ""
-			temp1 = serial_handler.initiateSerialConn(serial_port, baud_rate, timeout, "power?")
-			temp2 = serial_handler.initiateSerialConn(serial_port, baud_rate, timeout, "source?")
-			# Parse response
-			for c in temp1:
-				if c.isdigit():
-					power += c
-			for c in temp2:
-				if c.isdigit():
-					source += c
-			return power, source
-		power, source = getData()
-		# power, source = 100,100
-		if power == "timeout" or source == "timeout":
-			raise ValueError()
-		else:
-			print("Successfully retrieved Hub data:")
+class Daemon():
+	startTime = 0
+	endTime = 0
+	ID = 0
+	serial_port = ""
+	baud_rate = 0
+	timeout =0
+
+	def initializePortVars(self):
+		ID, serial_port, baud_rate, timeout = config.configure()
+		self.ID = ID
+		self.serial_port = serial_port
+		self.baud_rate = baud_rate
+		self.timeout = timeout
+
+	def runUpdate(self):
+		print("#############################################")
+		self.startTime = time.time()
+		self.initializePortVars()
+		print(datetime.datetime.now())
+		print("ID: %d, SERIAL_PORT: %s, BAUDRATE: %d, TIMEOUT: %0.2f" % (self.ID, self.serial_port, self.baud_rate, self.timeout))
+		print("Database update for Hub with ID %d initiated." % self.ID)
+		self.getHubData(self.ID, self.serial_port, self.baud_rate, self.timeout)
+
+	# Open serial port and query information
+	def getHubData(self, ID, serial_port, baud_rate, timeout):
+		print("Attempting to retrieve Hub and Pi data...")
+		try: 
+			def retrieveHubData():
+				power, source = "", ""
+				temp1 = serial_handler.initiateSerialConn(serial_port, baud_rate, timeout, "power?")
+				temp2 = serial_handler.initiateSerialConn(serial_port, baud_rate, timeout, "source?")
+				if temp1 == "error" or temp2 == "error":
+					raise ValueError
+				if temp1 == "timeout" or temp2 == "timeout":
+					raise ValueError
+				# Parse response
+				for c in temp1:
+					if c.isdigit():
+						power += c
+				for c in temp2:
+					if c.isdigit():
+						source += c
+				return power, source
+			def retrievePiData():
+				piData = pi_daemon.getSystemInfo()
+				return piData
+			power, source = retrieveHubData()	
+			piData = json.loads(retrievePiData())
+			system = piData["system"]
+			release = piData["release"]
+			version = piData["version"]
+			machine = piData["machine"]
+			processor = piData["processor"]
+			hostname = piData["hostname"]
+			temperature = piData["temperature"]
+			ip_address = piData["ip_address"]
+			print("Successfully retrieved Hub and Pi data:")
 			print("power = " + str(power))
 			print("source = " + str(source))
-			updateDatabase(ID, power, source)
-	except ValueError:
-		print("Something went wrong with the serial port. Unable to retrieve Hub data.")
-		print("Check unsuccessful.")
+			print("temperature = " + temperature)
+			self.updateDatabase(ID, power, source, temperature)
 
-# Open serial port and execute specified command
-def setHubSetting(serial_port, baud_rate, timeout, cmd):
-	try:
-		def putData():
-			output = ""
-			temp = serial_handler.initiateSerialConn(serial_port, baud_rate, timeout, cmd)
-			# Parse response
-			if temp == 'error':
-				return temp
-			for c in temp:
-				if c.isdigit():
-					output += c
-			return output
-		output = putData()
-		if output == "timeout":
-			raise ValueError
-		else:
-			return output
-	except:
-		print("error")
-		return "error"
+		except ValueError:
+			print("Something went wrong with the serial port. Unable to retrieve Hub data.")
+			print("Check unsuccessful.")
+			self.endTime = time.time()
+			totalTime = self.endTime - self.startTime
+			print("[completed in %.1fs]" % totalTime)
+			print("#############################################")
 
-# Update database with new info
-def updateDatabase(ID, power, source):
-	try: 
-		os.system("node db_check %d %s %s" % (ID, power, source))
-		print("Check successful.")
-	except:
-		print("Something went wrong with the database. Check unsuccessful.")
+	# Open serial port and execute specified command
+	def setHubSetting(self, serial_port, baud_rate, timeout, cmd):
+		try:
+			def putData():
+				output = ""
+				temp = serial_handler.initiateSerialConn(serial_port, baud_rate, timeout, cmd)
+				# Parse response
+				if temp == 'error':
+					return temp
+				for c in temp:
+					if c.isdigit():
+						output += c
+				return output
+			output = putData()
+			if output == "timeout":
+				raise ValueError
+			else:
+				return output
+		except:
+			print("error")
+			return "error"
+
+	# Update database with new info
+	def updateDatabase(self, ID, power, source, temperature):
+		try: 
+			subprocess.call(["node", "db_check", str(ID), str(power), str(source), temperature])
+			print("Check successful.")
+			self.endTime = time.time()
+			totalTime = self.endTime - self.startTime
+			print("[completed in %.1fs]" % totalTime)
+			print("#############################################")
+		except:
+			print("Something went wrong with the database. Check unsuccessful.")
+			self.endTime = time.time()
+			totalTime = self.endTime - self.startTime
+			print("[completed in %.1fs]" % totalTime)
+			print("#############################################")
 
 if __name__ == "__main__":
 	try:
 		cmd = sys.argv[1]
 		ID, serial_port, baud_rate, timeout = config.configure()
-		output = setHubSetting(serial_port, baud_rate, timeout, cmd)
+		daemon = Daemon()
+		output = daemon.setHubSetting(serial_port, baud_rate, timeout, cmd)
 		print(output)
 	except:
-		runUpdate()
+		daemon = Daemon()
+		daemon.runUpdate()
 
